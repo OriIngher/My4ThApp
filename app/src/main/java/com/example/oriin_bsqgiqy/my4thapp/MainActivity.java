4ddc84491bd1dcd886ef12fcf6d0a69b23a510c9
oriin_bsqgiqy <aa9803@bs.amalnet.k12.il>
2018-11-28 18:43:59 +0200

    זהו סיום התוכנית1

--- app/src/main/java/com/example/oriin_bsqgiqy/my4thapp/MainActivity.java
+++ app/src/main/java/com/example/oriin_bsqgiqy/my4thapp/MainActivity.java
@@ -23,9 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void go(View view) {
         button1.setText("This is a click number:"+x );
-        if(((x%10) == 7 ) || ((x%7) == 0))
+        if((x%7) == 0)
         {
             button1.setText("Boom!");
+        }
+        int xx = x;
+        for(;xx!=0; xx = xx/10)
+        {
+            if ((xx%10) == 7 )
+            {
+                button1.setText("Boom!");
+            }
         }
         x++ ;
     }
